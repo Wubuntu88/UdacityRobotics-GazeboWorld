88be96c502094e0162ba1215e149efd1863f9b14
William Gillespie <william.gillespie@soartech.com>
2020-12-06 15:41:41 -0500

    delete build.sh

--- build/build.sh
+++
@@ -1,4 +0,0 @@
-cmake ../
-make # You might get errors if your system is not up to date!
-# export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:/Users/williamgillespie/Documents/UdacityRobotics/UdacityRobotics-GazeboWorld/build
-export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:/home/workspace/UdacityRobotics-GazeboWorld/build
